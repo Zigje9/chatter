7c5530a2ba126295e597713f7e1e9f320dd1a135
jekoo <pjkwprn@gmail.com>
2021-02-27 23:21:01 +0900

    feat : login router 생성

--- back-end/chatter-be/app.js
+++ back-end/chatter-be/app.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const loginRouter = require('./routes/login')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(loginRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
