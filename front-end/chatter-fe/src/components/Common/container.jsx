25ee092bc237840da3af1721208b88590cdefb28
jekoo <pjkwprn@gmail.com>
2021-05-13 19:46:05 +0900

    feat : common container 구현

---
+++ front-end/chatter-fe/src/components/Common/container.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+`;
+
+const container = () => {
+  return <Div></Div>;
+};
+
+export default container;
---
+++ front-end/chatter-fe/src/components/LoginView/idForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 50px;
+  width: 50px;
+  margin-top: 10%;
+  background-color: green;
+`;
+
+const idForm = () => {
+  return <Container></Container>;
+};
+
+export default idForm;
