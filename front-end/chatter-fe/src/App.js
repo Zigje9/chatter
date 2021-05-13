0dacf6c139e06bedbfa0e42d8a7154da6179fe45
jekoo <pjkwprn@gmail.com>
2021-05-13 19:35:30 +0900

    feat : 로그인뷰 연결

--- front-end/chatter-fe/src/App.js
+++ front-end/chatter-fe/src/App.js
@@ -1,7 +1,17 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LoginView from './pages/LoginView';
 
 function App() {
-  return <div>CHATTER</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={LoginView}></Route>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ front-end/chatter-fe/src/pages/LoginView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginView = () => {
+  return (
+    <>
+      <div>로그인뷰</div>
+    </>
+  );
+};
+
+export default LoginView;
