5a24b832d09c0b4a20087abdea166d02031cac04
jekoo <pjkwprn@gmail.com>
2021-05-14 15:41:46 +0900

    feat : login submit axios 연동

--- front-end/chatter-fe/src/pages/LoginView.jsx
+++ front-end/chatter-fe/src/pages/LoginView.jsx
@@ -4,6 +4,7 @@ import IdForm from '../components/LoginView/idForm';
 import PwForm from '../components/LoginView/pwForm';
 import LoginButton from '../components/LoginView/loginButton';
 import ErrorMsg from '../utils/errorMsg';
+import { postAxios } from '../utils/axios';
 
 const LoginView = () => {
   const [id, setId] = useState('');
@@ -19,20 +20,21 @@ const LoginView = () => {
     setPw(e.target.value);
   };
 
-  const submitHandler = () => {
-    console.log(`${id}, ${pw}, here`);
-    // if (id.length === 0 || pw.length === 0) {
-    //   alert('아이디 또는 비밀번호를 입력해 주세요');
-    // } else {
-    //   const req = {
-    //     id: id,
-    //     pw: pw,
-    //   };
-    //   const res = await postAxios('login/', req);
-    //   if (res.data) {
-    //     alert(res.data);
-    //   }
-    // }
+  const submitHandler = async () => {
+    if (id.length === 0 || pw.length === 0) {
+      alert('아이디 또는 비밀번호를 입력해 주세요');
+    } else {
+      const req = {
+        id: id,
+        pw: pw,
+      };
+      try {
+        const res = await postAxios('login/', req);
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   return (
