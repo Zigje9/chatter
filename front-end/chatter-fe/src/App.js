06880122acc041883bab51d365cd2079eac96f81
jekoo <pjkwprn@gmail.com>
2021-05-17 00:23:24 +0900

    refactor : 인증 라우팅 변경

--- front-end/chatter-fe/src/App.js
+++ front-end/chatter-fe/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import GlobalStyle from './components/Common/globalStyle';
 import LoginView from './pages/LoginView';
 
 function App() {
+  // const defaultRouter = (
+  //   <>
+  //     <Route path="/login" component={LoginView}></Route>
+  //     <Redirect from="*" to="/login"></Redirect>
+  //   </>
+  // );
+
+  // const userRouter = (
+  //   <>
+  //     <Switch>
+  //       <Route exact path="/login" component={LoginView}></Route>
+  //     </Switch>
+  //   </>
+  // );
+
   return (
     <>
       <BrowserRouter>
+        {/* {user ? userRouter : defaultRouter} */}
         <Switch>
           <Route exact path="/login" component={LoginView}></Route>
         </Switch>
