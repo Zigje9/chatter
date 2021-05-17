a3aa5a9bec520fe183dc2401b41e141e374ef11f
jekoo <pjkwprn@gmail.com>
2021-05-17 22:34:11 +0900

    refactor : route 변경

--- front-end/chatter-fe/src/App.js
+++ front-end/chatter-fe/src/App.js
@@ -2,29 +2,16 @@ import React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import GlobalStyle from './components/Common/globalStyle';
 import LoginView from './pages/LoginView';
+import MainView from './pages/MainView';
 
 function App() {
-  // const defaultRouter = (
-  //   <>
-  //     <Route path="/login" component={LoginView}></Route>
-  //     <Redirect from="*" to="/login"></Redirect>
-  //   </>
-  // );
-
-  // const userRouter = (
-  //   <>
-  //     <Switch>
-  //       <Route exact path="/login" component={LoginView}></Route>
-  //     </Switch>
-  //   </>
-  // );
-
   return (
     <>
       <BrowserRouter>
-        {/* {user ? userRouter : defaultRouter} */}
         <Switch>
-          <Route exact path="/login" component={LoginView}></Route>
+          <Route path="/login" component={LoginView}></Route>
+          <Route path="/main" component={MainView}></Route>
+          <Redirect from="*" to="/main"></Redirect>
         </Switch>
         <GlobalStyle />
       </BrowserRouter>
