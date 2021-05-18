548a6728b2af16ed5ddaba9d6877c1749b8294ea
LeeJaeYun <ag502@naver.com>
2021-05-18 12:06:35 +0900

    refactor: container default props 추가

--- front-end/chatter-fe/src/components/Common/container.jsx
+++ front-end/chatter-fe/src/components/Common/container.jsx
@@ -6,10 +6,16 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: ${(props) => props.flexDirection};
-  height: ${(props) => props.height ? props.height: "100vh"};
-  width: ${(props) => props.width ? props.width: "100vw"};
-  background-color: ${(props) => props.bgColor ? props.bgColor : "none"};
+  height: ${(props) => props.height};
+  width: ${(props) => props.width};
+  background-color: ${(props) => props.bgColor};
 `;
+
+Container.defaultProps = {
+  height: "100vh",
+  width: "100vw",
+  bgColor: "none"
+}
 
 const container = ({ children, ...props }) => {
   return <Container {...props}>{children}</Container>;
