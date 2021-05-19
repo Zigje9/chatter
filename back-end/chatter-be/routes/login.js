1d18ac7b68a15d2738d3eabe0df28f2513e61463
jekoo <pjkwprn@gmail.com>
2021-05-19 22:27:17 +0900

    chore : restore

--- back-end/chatter-be/routes/login.js
+++ back-end/chatter-be/routes/login.js
@@ -10,13 +10,7 @@ router.post('/login', async (req, res) => {
   if (pw === result.user_password) {
     req.session.name = result.user_name;
     req.session.save(() => {
-      res.cookie("a", "b", {
-        httpOnly: true,
-        secure: false,
-        maxAge: 20000,
-        sameSite: "lax"
-      })
-      // res.cookie('user_name', req.session.name)
+      res.cookie('user_name', req.session.name)
       return res.status(200).send(req.session)
     })
   }
