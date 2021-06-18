ab19ca757c097c968f3ce5910bc9d6e1a66a4707
jekoo <pjkwprn@gmail.com>
2021-06-18 23:31:15 +0900

    feat :  make auth router

--- back-end/chatter-be/routes/auth.js
+++ back-end/chatter-be/routes/auth.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const sql = require('../model/db');
+const query = require('../model/query/user')
 const router = express.Router();
 
-router.get('/auth', (req, res) => {
+router.get('/auth', async (req, res) => {
   const sid = req.headers.cookies
   if (sid) {
-    
+    try {
+      const [result] = await sql(query.READ_SESSION_USER, sid)
+      const userName = result.data.cookie.name
+      return res.status(200).send(userName)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  else{
+    res.status(401).send("Unauthenticated")
   }
 })
 
