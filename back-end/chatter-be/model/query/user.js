231fad6ee504c2b20e22524edfcd91819db774db
jekoo <pjkwprn@gmail.com>
2021-06-18 23:24:40 +0900

    feat : make get session info query

--- back-end/chatter-be/model/query/user.js
+++ back-end/chatter-be/model/query/user.js
@@ -1,6 +1,7 @@
 const userQuery = {
   READ_USER: 'SELECT * FROM chatter_user WHERE user_id=?',
-  INSERT_USER: 'INSERT INTO chatter_user(user_id,user_name,user_password) VALUES(?,?,?)'
+  INSERT_USER: 'INSERT INTO chatter_user(user_id,user_name,user_password) VALUES(?,?,?)',
+  READ_SESSION_USER: 'SELECT * FROM sessions WHERE session_id=?'
 }
 
 module.exports = userQuery;
