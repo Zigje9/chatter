56029c97e4568c0915f19a7b7c39d627bd92fd88
jekoo <pjkwprn@gmail.com>
2021-06-18 23:23:41 +0900

    feat : authRouter get cookie

--- back-end/chatter-be/routes/auth.js
+++ back-end/chatter-be/routes/auth.js
@@ -1,11 +1,12 @@
-const express = require('express')
+const express = require('express');
+const sql = require('../model/db');
 const router = express.Router();
 
 router.get('/auth', (req, res) => {
-  console.log("쿠키 쿠키 쿠키", req.headers.cookie)
-  console.log("headerssss",req.headers)
-  console.log("header",req.header)
-  res.send("hhhhh")
+  const sid = req.headers.cookies
+  if (sid) {
+    
+  }
 })
 
 module.exports = router;
