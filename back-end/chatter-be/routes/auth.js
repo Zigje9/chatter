cffa28c5565e014ab0dab1e5cb124f14b98e2d4a
jekoo <pjkwprn@gmail.com>
2021-06-19 16:46:35 +0900

    chore : get parse data

--- back-end/chatter-be/routes/auth.js
+++ back-end/chatter-be/routes/auth.js
@@ -8,9 +8,7 @@ router.get('/auth', async (req, res) => {
   if (sid) {
     try {
       const [result] = await sql(query.READ_SESSION_USER, sid)
-      console.log("parse data@@@@@", JSON.parse(result.data))
-      const userName = result.data.cookie.name
-      console.log("userName", userName)
+      const userName = JSON.parse(result.data).name
       return res.status(200).send(userName)
     } catch (error) {
       console.log(error)
