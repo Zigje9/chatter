1e1fe89028c405fe2bda1e9d5c51d5fd8cda8550
jekoo <pjkwprn@gmail.com>
2021-06-23 19:05:51 +0900

    feat : make userList UI

---
+++ front-end/chatter-fe/src/components/UserList/userList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+import Container from '../Common/container';
+
+const UserList = () => {
+  return <Container></Container>;
+};
+
+export default UserList;
