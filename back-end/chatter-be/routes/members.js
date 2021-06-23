b788881bde1d67aaa7b1b4bb3800ac35c603396b
jekoo <pjkwprn@gmail.com>
2021-06-23 18:52:26 +0900

    chore : reformat res data

--- back-end/chatter-be/routes/members.js
+++ back-end/chatter-be/routes/members.js
@@ -5,9 +5,14 @@ const sql = require('../model/db')
 const query = require('../model/query/user')
 
 router.get('/members', async (req, res) => {
-  const [result] = await sql(query.READ_ALL_USER)
-  console.log(result.data)
-  return res.status(200).json({"hi":1})
+  try {
+    const result = await sql(query.READ_ALL_USER)
+    const resData = {}
+    result.map((user) => resData[user.user_name] = user.user_id)
+    return res.status(200).json(resData)    
+  } catch (error) {
+    console.log(error) 
+  }
 })
 
 module.exports = router;
