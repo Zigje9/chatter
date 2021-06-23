c265b7251ae488724a68f87d964fcad9567c7538
jekoo <pjkwprn@gmail.com>
2021-06-23 18:23:59 +0900

    feat : make getUserList function

--- front-end/chatter-fe/src/pages/MainView.jsx
+++ front-end/chatter-fe/src/pages/MainView.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from '../components/Common/container';
 import { useSelector } from 'react-redux';
+import { getAxios } from '../utils/axios';
 
 /* 
 TODO
@@ -13,10 +14,30 @@ TODO
 
 const MainView = () => {
   const userName = useSelector((state) => state.user.userName);
+
+  const userList = {};
+
+  const getUserList = async () => {
+    try {
+      const res = await getAxios('members/');
+      console.log('res', res);
+      console.log('data', res.data);
+      console.log('members', res.data.members);
+      res.data.members.map((e) => (userList[e.userName] = e.userId));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUserList();
+  }, []);
+
   return (
     <Container flexDirection="row">
       <div>메인 페이지</div>
       <h2>{userName}</h2>
+      <h3>{userList}</h3>
     </Container>
   );
 };
