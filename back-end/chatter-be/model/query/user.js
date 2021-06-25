3597a6e419a7df2a970d2a80a773d985264bb913
jekoo <pjkwprn@gmail.com>
2021-06-25 17:55:41 +0900

    chore : modify user query

--- back-end/chatter-be/model/query/user.js
+++ back-end/chatter-be/model/query/user.js
@@ -1,6 +1,6 @@
 const userQuery = {
   READ_USER: 'SELECT * FROM chatter_user WHERE user_id=?',
-  INSERT_USER: 'INSERT INTO chatter_user(user_id,user_name,user_password) VALUES(?,?,?)',
+  INSERT_USER: 'INSERT INTO chatter_user(user_id,user_name,user_password,user_profile) VALUES(?,?,?,?)',
   READ_SESSION_USER: 'SELECT * FROM sessions WHERE session_id=?',
   READ_ALL_USER: 'SELECT * FROM chatter_user'
 }
