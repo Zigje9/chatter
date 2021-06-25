ee48004f0149898c77e108c0e9c665bf17f97459
jekoo <pjkwprn@gmail.com>
2021-06-25 18:32:04 +0900

    chore : change export module

--- back-end/chatter-be/utils/getRandom.js
+++ back-end/chatter-be/utils/getRandom.js
@@ -1,5 +1,7 @@
-export const getRandom = () => {
+const getRandom = () => {
   let min = Math.ceil(1);
   let max = Math.floor(11);
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+module.exports = getRandom;
