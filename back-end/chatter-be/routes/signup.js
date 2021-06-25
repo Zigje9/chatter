f5d69d77762f37d2fc7a255fe071f1f2779070fc
jekoo <pjkwprn@gmail.com>
2021-06-25 17:57:20 +0900

    feat : apply random profile idx

--- back-end/chatter-be/routes/signup.js
+++ back-end/chatter-be/routes/signup.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const sql = require('../model/db')
 const query = require('../model/query/user')
+const random = require('../utils/getRandom')
 
 router.post('/signup', async (req, res) => {
   const {id, pw, userName} = req.body;
@@ -13,7 +14,8 @@ router.post('/signup', async (req, res) => {
     return res.status(200).send("가입된 회원입니다.")
   }
   else{
-    await sql(query.INSERT_USER, [id, userName, pw])
+    const profile_idx = random()
+    await sql(query.INSERT_USER, [id, userName, pw, profile_idx])
     res.send("회원가입 완료.")
   }
 })
