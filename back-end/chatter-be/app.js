bceaae32f1b670ef0a663002c9f9bc021f0d1517
jekoo <pjkwprn@gmail.com>
2021-06-26 14:45:38 +0900

    feat : install socket io

--- back-end/chatter-be/app.js
+++ back-end/chatter-be/app.js
@@ -1,49 +1,59 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const dbConfig = require('./model/config')
-const session = require('express-session')
-const MySQLStore = require('express-mysql-session')(session)
-const bodyParser = require('body-parser'); 
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const app = express();
+const port = 5000;
+const dbConfig = require('./model/config');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const corsOptions = {
   origin: 'http://localhost:3000',
-  credentials: true
-}
+  credentials: true,
+};
 
-app.use(bodyParser.urlencoded({extended:true})); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors(corsOptions))
-app.use(cookieParser())
+app.use(cors(corsOptions));
+app.use(cookieParser());
 
-const loginRouter = require('./routes/login')
-const logoutRouter = require('./routes/logout')
-const signupRouter = require('./routes/signup')
-const authRouter = require('./routes/auth')
-const memberRouter = require('./routes/members')
+const loginRouter = require('./routes/login');
+const logoutRouter = require('./routes/logout');
+const signupRouter = require('./routes/signup');
+const authRouter = require('./routes/auth');
+const memberRouter = require('./routes/members');
 
-const sessionStore = new MySQLStore(dbConfig)
+const sessionStore = new MySQLStore(dbConfig);
 
-app.use(session({
-  secret: '!@#$%^&',
-  store: sessionStore,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-    maxAge: 500000
-  }
-}))
+app.use(
+  session({
+    secret: '!@#$%^&',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 500000,
+    },
+  }),
+);
 
-app.use(loginRouter)
-app.use(logoutRouter)
-app.use(signupRouter)
-app.use(authRouter)
-app.use(memberRouter)
+app.use(loginRouter);
+app.use(logoutRouter);
+app.use(signupRouter);
+app.use(authRouter);
+app.use(memberRouter);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const server = app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+const listen = require('socket.io');
+const io = listen(server);
+io.on('connection', function (socket) {
+  console.log('socket connection!!!!!!');
+  socket.on('client message', function (data) {
+    io.emit('server message', data.message);
+  });
+});
