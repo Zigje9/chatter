ef815d90352979d25c45e671db9dba4a74ea5e47
jekoo <pjkwprn@gmail.com>
2021-06-30 15:53:03 +0900

    chore : add userId

--- back-end/chatter-be/routes/login.js
+++ back-end/chatter-be/routes/login.js
@@ -1,23 +1,23 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
-const sql = require('../model/db')
-const query = require('../model/query/user')
+const sql = require('../model/db');
+const query = require('../model/query/user');
 
 router.post('/login', async (req, res) => {
-  const {id, pw} = req.body;
-  const [result] = await sql(query.READ_USER, id)
+  const { id, pw } = req.body;
+  const [result] = await sql(query.READ_USER, id);
   if (pw === result.user_password) {
+    req.session.userId = result.user_id;
     req.session.name = result.user_name;
     req.session.profile = result.user_profile;
     req.session.save(() => {
-      req.session.sid = req.sessionID
-      return res.status(200).send(req.session)
-    })
+      req.session.sid = req.sessionID;
+      return res.status(200).send(req.session);
+    });
+  } else {
+    res.status(401).send('login failed');
   }
-  else{
-    res.status(401).send("login failed")
-  }
-})
+});
 
 module.exports = router;
