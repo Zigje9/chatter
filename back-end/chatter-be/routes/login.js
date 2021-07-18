149b21956ef61db8ed3d76eb70ab173d59fcfaff
jekoo <pjkwprn@gmail.com>
2021-07-18 18:14:11 +0900

    feat : password crypto

--- back-end/chatter-be/routes/login.js
+++ back-end/chatter-be/routes/login.js
@@ -1,27 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const crypto = require('crypto');
+require('dotenv').config();
 
 const sql = require('../model/db');
 const query = require('../model/query/user');
 
-router.post('/login', async (req, res) => {
-  if (req.session.userId) {
-    console.log(req.session.userId);
-    res.status(401).send('login failed');
-  } else {
-    const { id, pw } = req.body;
+const getSaltPw = (id, pw) => {
+  return new Promise(async (resolve, reject) => {
     const [result] = await sql(query.READ_USER, id);
-    if (pw === result.user_password) {
-      req.session.userId = result.user_id;
-      req.session.name = result.user_name;
-      req.session.profile = result.user_profile;
-      req.session.save(() => {
-        req.session.sid = req.sessionID;
-        return res.status(200).send(req.session);
-      });
-    } else {
-      res.status(401).send('login failed');
-    }
+    const salt = result.user_salt;
+    crypto.pbkdf2(pw, salt, process.env.SALT_NUM, 64, 'sha512', (err, key) => {
+      if (err) reject(err);
+      resolve(key.toString('base64'));
+    });
+  });
+};
+
+router.post('/login', async (req, res) => {
+  const { id, pw } = req.body;
+  const saltPassword = await getSaltPw(id, pw);
+  const [result] = await sql(query.READ_USER, id);
+  const originPassword = result.user_password;
+  if (saltPassword === originPassword) {
+    req.session.userId = result.user_id;
+    req.session.name = result.user_name;
+    req.session.profile = result.user_profile;
+    req.session.save(() => {
+      req.session.sid = req.sessionID;
+      return res.status(200).send(req.session);
+    });
+  } else {
+    res.status(401).send('login failed');
   }
 });
 
