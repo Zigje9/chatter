223d4db25a1b444dab95a90018af9a6cdb18bb35
jekoo <pjkwprn@gmail.com>
2021-07-19 23:28:11 +0900

    chore : fix query error

--- back-end/chatter-be/model/query/log.js
+++ back-end/chatter-be/model/query/log.js
@@ -1,9 +1,9 @@
 const logQuery = {
-  INSERT_PUBLIC_LOG: 'INSERT INTO public_log(message,user_id,date) ORDER BY date VALUES(?,?,?)',
+  INSERT_PUBLIC_LOG: 'INSERT INTO public_log(message,user_id,date) VALUES(?,?,?)',
   INSERT_PRIVATE_LOG:
-    'INSERT INTO private_log(room_id,message,from_id,to_id,date) ORDER BY date VALUES(?,?,?,?,?)',
+    'INSERT INTO private_log(room_id,message,from_id,to_id,date) VALUES(?,?,?,?,?)',
   READ_PUBLIC_LOG:
-    'SELECT message, user_id, user_name, date FROM public_log natural JOIN chatter_user',
+    'SELECT message, user_id, user_name, date FROM public_log natural JOIN chatter_user ORDER BY date',
 };
 
 module.exports = logQuery;
